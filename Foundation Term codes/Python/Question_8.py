7561ae2ee21ed495ad902b60e78d84ad103dbef5
Sanyal <sandipto.sanyal@accenture.com>
2020-04-04 22:41:25 +0530

    Distributions

--- Foundation Term codes/Python/Question_8.py
+++ Foundation Term codes/Python/Question_8.py
@@ -4,6 +4,7 @@ Name: Sandipto Sanyal
 PGID: 12010004
 """
 import string
+import re
 class FileCleaner:
     """This class will perform the following:
         1. breaks each line into words 
@@ -22,40 +23,36 @@ class FileCleaner:
         with open(path, 'r') as file:
             self.file_content = file.read()
     
-    def break_each_line(self, string_of_char):
-        """Reads a string of characters and break each line
+    def clean_text(self, string_of_char):
+        """Reads a string of characters and break each line 
         Parameters:
             string_of_char: string
             String of characters to break each line by '.'
         Returns:
             List of sentences
         """
-        list_of_sentences = string_of_char.split('.')
-        return list_of_sentences
+        list_of_sentences = string_of_char.split('.')[:-1]
+        # break each line into words
+        list_of_words = []
+        for sentences in list_of_sentences:
+            # remove punctuations
+            sentences = sentences.translate(str.maketrans('', '', string.punctuation))
+            # remove line feed characters
+            sentences = re.sub('\n+', ' ', sentences)
+            # strip white spaces from first to last
+            sentences = sentences.strip() 
+            # remove extra spaces within each word from the sentences
+            sentences = re.sub(' +', ' ', sentences)
+            # convert to upper case
+            sentences = sentences.upper()
+            # break the sentences into list of words
+            list_of_words.append(sentences.split(' '))
+        return list_of_words
     
-    def convert_sentences(self, list_of_sentences):
-        """Convert the sentences as below:
-            1. strips whitespace and punctuation from the words
-            2. converts them to uppercase
-        Parameters:
-            list_of_sentences: list
-            List of sentences separated by new line from our file
-        Returns:
-            List of sentences with extra white spaces stripped
-        """
-        for i, sentences in enumerate(list_of_sentences):
-            sentences = sentences.strip() # strip white spaces
-            sentences = sentences.translate(str.maketrans('', '', string.punctuation)) # remove punctuations
-            sentences = sentences.upper() # convert to upper case
-            list_of_sentences[i] = sentences
-        return list_of_sentences
-
 if __name__=='__main__':
     path = r'datasets/Q8.txt'
     from Question_8 import FileCleaner
     fc = FileCleaner(path=path)
     string_of_char = fc.file_content
-    list_of_sentences = fc.break_each_line(string_of_char)
-    list_of_sentences = fc.convert_sentences(list_of_sentences)
-    print("The cleaned text::::")
-    print(' '.join(list_of_sentences))
+    list_of_words = fc.clean_text(string_of_char)
+    print('List of words\n{}'.format(list_of_words))
--- Foundation Term codes/Python/Question_9.py
+++ Foundation Term codes/Python/Question_9.py
@@ -4,3 +4,30 @@ Name: Sandipto Sanyal
 PGID: 12010004
 """
 
+import os
+
+class DirectorySearch:
+    
+    def __init__(self, directory, suffix):
+        self.rootdir = directory
+        self.suffix = suffix
+    
+    def walk_directory(self):
+        list_of_files_with_suffix = []
+        for root, subdirs, files in os.walk(self.rootdir):
+            for filename in files:
+                # check whether the given suffix = the suffix in the file name
+                # os.path.splitext takes care of filename where last characters
+                # after '.' denotes the file extension/suffixl
+                filename_without_suffix, file_extension = os.path.splitext(filename)
+                if self.suffix == file_extension:
+                    list_of_files_with_suffix.append(os.path.join(root,filename))
+        return list_of_files_with_suffix
+
+if __name__ == '__main__':
+    print('Provide the directory name to scan: ')
+    directory = input()
+    print('Provide the suffix of the file names to scan: ')
+    suffix = input()
+    ds = DirectorySearch(directory, suffix)
+    list_of_files_with_suffix = ds.walk_directory()
